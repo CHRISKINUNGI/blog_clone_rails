f7bd3f75296083198c71153e87e0fce617886def
CHRISKINUNGI <chrskinungi@gmail.com>
2023-11-09 03:39:50 +0300

    commit to add articles features and New article

---
+++ blog/app/controllers/articles_controller.rb
@@ -0,0 +1,31 @@
+class ArticlesController < ApplicationController
+    def index 
+        # fetch all articles from the database
+        @articles = Article.all
+    end
+
+    def show 
+        #
+        @article = Article.find(params[:id])
+    end
+
+    def new 
+        @article = Article.new 
+    end
+
+    def create 
+        @article = Article.new(title: "...", body: "...")
+
+        if @article.save 
+            redirect_to @article
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    private
+        def article_params 
+            params.require(:article).permit(:title, :body)
+        end
+
+end
